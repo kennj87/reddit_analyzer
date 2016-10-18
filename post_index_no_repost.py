6eaad45daa85ac31e10e1598f48d2e4878f75aba
kennj87 <mail@kennj.com>
2016-10-18 14:31:19 +0200

    ready

---
+++ post_index_no_repost.py
@@ -0,0 +1,31 @@
+from database import cursor
+from database import db
+
+def create_posts_no_reposts(id):
+    if id == None:
+        id = 0
+    sql_select = "SELECT MIN(ID),url FROM `post_info` WHERE ID > %s GROUP BY URL ORDER BY MIN(ID) ASC" % (id)
+    try:
+        cursor.execute(sql_select)
+        results = cursor.fetchall()
+        for row in results:
+            sql_insert = "INSERT INTO post_no_repost (original_id, url) VALUES ('%s', '%s')" % (row[0], row[1])
+            try:
+                cursor.execute(sql_insert)
+            except:
+                db.rollback()
+        db.commit()
+    except:
+        print("Unable to find data")
+
+def find_newest_post_no_repost():
+    sql_select = "SELECT MAX(original_id) FROM post_no_repost"
+    try:
+        cursor.execute(sql_select)
+        result = cursor.fetchall()
+        for row in result:
+            create_posts_no_reposts(row[0])
+    except:
+        pass
+find_newest_post_no_repost()
+db.close()
