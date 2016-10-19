a2d91b97fc36f69cb1a68abd40ae4fe109681ff1
kennj87 <mail@kennj.com>
2016-10-19 02:09:34 +0200

    added repost c hecks

--- repost_index.py
+++ repost_index.py
@@ -25,6 +25,28 @@ def check_dupes_all():
     except:
         pass
 
+def check_dupes_update():
+    sql_select = "SELECT url FROM post_reposts"
+    try:
+        cursor.execute(sql_select)
+        results = cursor.fetchall()
+        for row in results:
+            sql_get_amount = "SELECT COUNT(url) from post_info where url = '%s'" % (row[0])
+            try:
+                cursor.execute(sql_get_amount)
+                result = cursor.fetchall()
+                for rows in result:
+                    sql_update = "UPDATE post_reposts SET repost_count = '%s' WHERE url = '%s'" % (rows[0]-1, row[0])
+                    try:
+                        cursor.execute(sql_update)
+                    except:
+                        pass
+                    db.commit()
+            except:
+                pass
+
+    except:
+        pass
 
 def check_dupes_same_subreddit():
     sql_dupes = "SELECT y.id,y.author,y.url,y.subreddit FROM post_info y INNER JOIN (SELECT url, COUNT(*) AS counter FROM post_info GROUP BY url HAVING COUNT(*)>1) dt ON y.url=dt.url AND y.subreddit=dt.subreddit"
@@ -35,5 +57,7 @@ def check_dupes_same_subreddit():
             print(row)
     except:
         pass
+
 check_dupes_all()
+check_dupes_update()
 db.close()
