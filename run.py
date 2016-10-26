9ceddb221d32dee3d43a56f0cfcd25b9fee12bab
kennj87 <mail@kennj.com>
2016-10-26 15:37:21 +0200

    Updated controller

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 from scrab_reddit import runs
+from database import db,cursor
+import threading
 
-runs(100000)
+def see_if_it_runs():
+    if check_if_run():
+        runs()
+        print("ran scrab")
+    threading.Timer(1.0, see_if_it_runs).start()
+
+def check_if_run():
+    sql = "SELECT next_run FROM controller WHERE function = 'run'"
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        db.commit()
+        for row in result:
+            if row[0] == 1:
+                return True
+            elif row[0] != 1:
+                return False
+    except:
+        pass
+
+see_if_it_runs()
--- scrab_reddit.py
+++ scrab_reddit.py
@@ -6,61 +6,53 @@ from settings import *
 from database import db
 from database import cursor
 
-def runs(runs):
-    runcount = 0
-    runrep = 1
-    while runrep == 1:
-        r = praw.Reddit(user_agent = USER_AGENT)
-        oauth_helper = PrawOAuth2Mini(r,app_key = APP_KEY,app_secret = APP_SECRET,
-                                        access_token = ACCESS_TOKEN,scopes = SCOPES,
-                                        refresh_token = REFRESH_TOKEN)
-        oauth_helper.refresh()
-        subreddit = r.get_subreddit('all')
+def runs():
+    r = praw.Reddit(user_agent = USER_AGENT)
+    oauth_helper = PrawOAuth2Mini(r,app_key = APP_KEY,app_secret = APP_SECRET,
+                                    access_token = ACCESS_TOKEN,scopes = SCOPES,
+                                    refresh_token = REFRESH_TOKEN)
+    oauth_helper.refresh()
+    subreddit = r.get_subreddit('all')
 
-        author = "none"
-        created = 0
-        over_18 = 0
-        permalink = "none"
-        subred = "none"
-        thumbnail = "none"
-        title = "none"
-        url = "none"
-        name = "none"
-        media = "none"
+    author = "none"
+    created = 0
+    over_18 = 0
+    permalink = "none"
+    subred = "none"
+    thumbnail = "none"
+    title = "none"
+    url = "none"
+    name = "none"
+    media = "none"
 
-        def id_used(name):
-            sql_get = "SELECT name FROM post_info WHERE name = '%s'" % (name)
+    def id_used(name):
+        sql_get = "SELECT name FROM post_info WHERE name = '%s'" % (name)
+        try:
+            cursor.execute(sql_get)
+            if not cursor.rowcount:
+                return True
+            else:
+                return False
+        except:
+            pass
+
+    for post in reversed(list(subreddit.get_new(after=name_id))):
+        author = str(vars(post)['author'])
+        created = int(vars(post)['created'])
+        over_18 = int(vars(post)['over_18'])
+        permalink = str(vars(post)['permalink'])
+        subred = str(vars(post)['subreddit'])
+        thumbnail = str(vars(post)['thumbnail'])
+        title = str(vars(post)['title'])
+        url = str(vars(post)['url'])
+        name = str(vars(post)['name'])
+        media = str(vars(post)['media'])
+        if id_used(name):
+            sql = "INSERT INTO `post_info` (`ID`, `author`, `title`, `subreddit`, `url`, `permalink`, `media`, `created`, `thumbnail`, `over_18`, `name`) " \
+                "VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
+                (author, title, subred, url, permalink, media, created, thumbnail, over_18, name)
             try:
-                cursor.execute(sql_get)
-                if not cursor.rowcount:
-                    return True
-                else:
-                    return False
+                cursor.execute(sql)
+                db.commit()
             except:
-                pass
-
-        for post in reversed(list(subreddit.get_new(after=name_id))):
-            author = str(vars(post)['author'])
-            created = int(vars(post)['created'])
-            over_18 = int(vars(post)['over_18'])
-            permalink = str(vars(post)['permalink'])
-            subred = str(vars(post)['subreddit'])
-            thumbnail = str(vars(post)['thumbnail'])
-            title = str(vars(post)['title'])
-            url = str(vars(post)['url'])
-            name = str(vars(post)['name'])
-            media = str(vars(post)['media'])
-            if id_used(name):
-                sql = "INSERT INTO `post_info` (`ID`, `author`, `title`, `subreddit`, `url`, `permalink`, `media`, `created`, `thumbnail`, `over_18`, `name`) " \
-                    "VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
-                    (author, title, subred, url, permalink, media, created, thumbnail, over_18, name)
-                try:
-                    cursor.execute(sql)
-                    db.commit()
-                except:
-                    db.rollback()
-        runcount = runcount +1
-        print(runcount)
-        if runcount >= runs:
-            runrep = 0
-    db.close()
+                db.rollback()
